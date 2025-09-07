c5c8410d34e3f01da0e4ba60feec427d85d10254
Rohan Maan <rohanmandal2208@gmail.com>
2025-09-07 12:38:02 +0530

    day2 Update

--- ChatBotWithLang/chatbot.js
+++ ChatBotWithLang/chatbot.js
@@ -1,28 +1,28 @@
-// // chatbot.js
-// import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-// import "dotenv/config";
-// import { chatbotTemplate } from "./prompt.js";
-// import { ConversationChain } from "langchain/chains";
-// import { BufferMemory } from "langchain/memory";
+// chatbot.js
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import "dotenv/config";
+import { chatbotTemplate } from "./prompt.js";
+import { ConversationChain } from "langchain/chains";
+import { BufferMemory } from "langchain/memory";
 
-// const model = new ChatGoogleGenerativeAI({
-//   model: "gemini-1.5-flash",
-//   apiKey: process.env.GEMINI_API_KEY,
-// });
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-1.5-flash",
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
-// // ✅ Memory setup
-// const memory = new BufferMemory();
+// ✅ Memory setup
+const memory = new BufferMemory();
 
-// // ✅ Chain setup (model + memory + prompt)
-// const chain = new ConversationChain({
-//   llm: model,
-//   memory: memory,
-//   prompt: chatbotTemplate,
-// });
+// ✅ Chain setup (model + memory + prompt)
+const chain = new ConversationChain({
+  llm: model,
+  memory: memory,
+  prompt: chatbotTemplate,
+});
 
-// export async function askBot(question) {
-//   const response = await chain.call({
-//     question,
-//   });
-//   return response.response;
-// }
+export async function askBot(question) {
+  const response = await chain.call({
+    question,
+  });
+  return response.response;
+}
